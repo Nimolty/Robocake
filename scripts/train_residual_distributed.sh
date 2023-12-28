266d1144e7ca7191461e665f4d738863c05fce25
nimol <v100v3>
2023-12-28 23:47:27 +0800

    train_residual

--- scripts/train_residual_distributed.sh
+++ scripts/train_residual_distributed.sh
@@ -13,31 +13,66 @@
 #   --num_worker 8
 
 ########################## on going ##########################
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port 30210 /home/tianyang/Robocake/train_residual_distributed.py \
-  --stage dy \
-	--data_type ngrip_fixed \
-	--residual_n_epoch 5000 \
-	--ckp_per_iter 500 \
-  --wandb_vis_log_per_iter 200 \
-  --dataf /nvme/tianyang/residual_robocake_data/data_ngrip_fixed \
-  --outf /nvme/tianyang/residual_robocake_data/dump_ngrip_fixed \
-  --resume_prior_path /nvme/tianyang/residual_robocake_data/dump_ngrip_fixed_prior_multi/0/prior_net_epoch_6_iter_374/prior_model.pth \
-  --exp_id 1 \
-  --experiment_name residual_model_v100v3 \
-  --batch_size 8 \
-  --num_worker 8
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port 30210 /home/tianyang/Robocake/train_residual_distributed.py \
+#   --stage dy \
+# 	--data_type ngrip_fixed \
+# 	--residual_n_epoch 5000 \
+# 	--ckp_per_iter 500 \
+#   --wandb_vis_log_per_iter 200 \
+#   --dataf /nvme/tianyang/residual_robocake_data/data_ngrip_fixed \
+#   --outf /nvme/tianyang/residual_robocake_data/dump_ngrip_fixed \
+#   --resume_prior_path /nvme/tianyang/residual_robocake_data/dump_ngrip_fixed_prior_multi/0/prior_net_epoch_6_iter_374/prior_model.pth \
+#   --exp_id 1 \
+#   --experiment_name residual_model_v100v3 \
+#   --batch_size 8 \
+#   --num_worker 8
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 30211 /home/tianyang/Robocake/train_residual_distributed.py \
+# CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 30211 /home/tianyang/Robocake/train_residual_distributed.py \
+#   --stage dy \
+# 	--data_type ngrip_fixed \
+# 	--residual_n_epoch 5000 \
+# 	--ckp_per_iter 500 \
+#   --wandb_vis_log_per_iter 200 \
+#   --dataf /nvme/tianyang/residual_robocake_data/data_ngrip_fixed \
+#   --outf /nvme/tianyang/residual_robocake_data/dump_ngrip_fixed \
+#   --resume_prior_path /home/tianyang/Robocake/prior/dump_ngrip_fixed_desktop/3/prior_net_epoch_99_iter_1325/prior_model.pth \
+#   --exp_id 2 \
+#   --experiment_name residual_model_v100v3 \
+#   --batch_size 8 \
+#   --num_worker 8 \
+#   --remove_his_particles 1
+
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port 30211 /home/tianyang/Robocake/train_residual_distributed.py \
   --stage dy \
 	--data_type ngrip_fixed \
 	--residual_n_epoch 5000 \
-	--ckp_per_iter 500 \
+	--ckp_per_iter 1500 \
   --wandb_vis_log_per_iter 200 \
   --dataf /nvme/tianyang/residual_robocake_data/data_ngrip_fixed \
   --outf /nvme/tianyang/residual_robocake_data/dump_ngrip_fixed \
   --resume_prior_path /home/tianyang/Robocake/prior/dump_ngrip_fixed_desktop/3/prior_net_epoch_99_iter_1325/prior_model.pth \
-  --exp_id 2 \
-  --experiment_name residual_model_v100v3 \
+  --exp_id 3 \
+  --experiment_name residual_model_v100v3_exp3 \
   --batch_size 8 \
   --num_worker 8 \
-  --remove_his_particles 1
+  --residual_input_next_action ZERO \
+  --ecal_ckp_per_iter 1 \
+  --vis None
+
+CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 30212 /home/tianyang/Robocake/train_residual_distributed.py \
+  --stage dy \
+	--data_type ngrip_fixed \
+	--residual_n_epoch 5000 \
+	--ckp_per_iter 1500 \
+  --wandb_vis_log_per_iter 200 \
+  --dataf /nvme/tianyang/residual_robocake_data/data_ngrip_fixed \
+  --outf /nvme/tianyang/residual_robocake_data/dump_ngrip_fixed \
+  --resume_prior_path /home/tianyang/Robocake/prior/dump_ngrip_fixed_desktop/3/prior_net_epoch_99_iter_1325/prior_model.pth \
+  --exp_id 4 \
+  --experiment_name residual_model_v100v3_exp4 \
+  --batch_size 8 \
+  --num_worker 8 \
+  --residual_input_next_action LAST_GT \
+  --ecal_ckp_per_iter 1 \
+  --vis None
